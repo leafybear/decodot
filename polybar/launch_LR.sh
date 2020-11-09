63859acf789cd68a95479c897ea37eb155342efd
Amy Bentley <leafybear@icloud.com>
2020-11-10 10:01:52 +1100

    begin new polybar layout. alter picom blur.

---
+++ polybar/launch_LR.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bars
+polybar left >> /tmp/polybar1.log 2>&1 & disown
+polybar right >> /tmp/polybar1.log 2>&1 & disown
+
+echo "Bars launched..."
