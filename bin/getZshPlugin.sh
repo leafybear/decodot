bfed16577c070121b7e2ff2f7b20e078727e717e
Amy Bentley <leafybear@icloud.com>
2020-11-21 17:42:27 +1100

    update sublime text; update zshrc

---
+++ bin/getZshPlugin.sh
@@ -0,0 +1,6 @@
+# syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# autocomplete suggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# k, the better ls
+git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
