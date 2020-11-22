78ca0b1a8c4826482ed1305fed7b52b408e0f2a7
Amy Bentley <leafybear@icloud.com>
2020-11-22 23:54:27 +1100

    moved scripts into better locations

--- shell/getZshPlugin.sh
+++ shell/getZshPlugin.sh
@@ -3,4 +3,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 # autocomplete suggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # k, the better ls
-git clone https://github.com/supercrabtree/k $ZSH_CUSTOM/plugins/k
+git clone https://github.com/supercrabtree/k ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/k
