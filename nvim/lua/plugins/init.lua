9bec91cd44e0020bf07927f104239c7c2782e8c9
Amy Bentley <leafybear@icloud.com>
2021-08-29 16:22:09 +1000

    stage 1 of lua plugin configs for neovim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -27,81 +27,24 @@ return packer.startup(function()
 	use {'wbthomason/packer.nvim',
             event = "VimEnter" }
  
-	-- -- NNN file manager inside of neovim
- 	-- use { 'mcchrish/nnn.vim',
- 		-- config = function() require "plugins.nnn" end }
+	-- NNN file manager inside of neovim
+ 	use { 'mcchrish/nnn.vim',
+ 		config = function() require "plugins.nnn" end }
 		
 	-- Simple line commenting
 	use { "terrortylor/nvim-comment",
 		cmd = "CommentToggle",
 		config = function() require "plugins.comment" end }
 
-	-- -- Extra lua functions needed by some plugins
-	-- use { "nvim-lua/plenary.nvim",
-	-- 	event = "BufRead" }
-	
-	-- -- Small popup window library
-	-- use { "nvim-lua/popup.nvim",
-	-- 	after = "plenary.nvim" }
-    
-	-- -- color related stuff
-	-- use { "siduck76/nvim-base16.lua",
-        -- after = "packer.nvim",
-        -- config = function() require "themes.theme" end }
+	-- advanced syntax highlighting
+	use { "nvim-treesitter/nvim-treesitter",
+		event = "BufRead",
+		config = function() require "plugins.treesitter" end }
 
-	-- -- file icons
-    -- use { "kyazdani42/nvim-web-devicons",
-    	-- after = "nvim-base16.lua",
-        -- config = function() require "plugins.icons" end }
-
-	-- -- advanced syntax highlighting
-	-- use { "nvim-treesitter/nvim-treesitter",
+	-- -- preview colors in code
+	-- use { "norcalli/nvim-colorizer.lua",
 	-- 	event = "BufRead",
-	-- 	config = function() require "plugins.treesitter" end }
-
--- -- PART 2
-
-	-- -- fancy tab bar
-    -- use { "akinsho/nvim-bufferline.lua",
-	-- 	after = "nvim-base16.lua",
-	-- 	config = function() require "plugins.bufferline" end }
-    
-	-- -- fancy status bar
-	-- use { "glepnir/galaxyline.nvim",
-	-- 	after = "nvim-base16.lua",
-	-- 	config = function() require "plugins.statusline" end }
-	
-	-- -- fuzzy file finder window
-	-- use { "nvim-telescope/telescope.nvim",
-	-- 	cmd = "Telescope",
-	-- 	config = function() require "plugins.telescope" end }
-
-        -- -- use { "nvim-telescope/telescope-fzf-native.nvim",
-        -- --     run = "make",
-        -- --     cmd = "Telescope" }
-        -- -- use { "nvim-telescope/telescope-media-files.nvim",
-        -- --     cmd = "Telescope" }
-	
-    -- -- file picker dashboard
-	-- use { "glepnir/dashboard-nvim",
-	-- 	cmd = { "Dashboard",
-	-- 		"DashboardNewFile",
-	-- 		"DashboardJumpMarks",
-	-- 		"SessionLoad",
-	-- 		"SessionSave" },
-	-- 	setup = function() require "plugins.dashboard" end }
-
-	-- -- distraction free editing modes
-	-- use { "Pocco81/TrueZen.nvim",
-	-- 	cmd = { "TZAtaraxis",
-	-- 			"TZMinimalist",
-	-- 			"TZFocus" },
-	-- 	config = function() require "plugins.zenmode" end }
-
-	-- -- alternative to telescope for when it doesnt work
-	-- use { "liuchengxu/vim-clap" }
-
--- -- PART 3
+	-- 	config = function() require "plugins.colorizer" end }
 
 	-- -- show whitespace characters and indentation
 	-- use { "lukas-reineke/indent-blankline.nvim",
@@ -115,11 +58,55 @@ return packer.startup(function()
 
 	-- -- git status marks in gutter
 	-- use "airblade/vim-gitgutter"
+    
+	-- file icons
+    use { "kyazdani42/nvim-web-devicons" }
+    --	config = function() require "plugins.icons" end }
+	
+	use { 'hoob3rt/lualine.nvim',
+		config = function() require "plugins.lualine" end }
 
-	-- -- preview colors in code
-	-- use { "norcalli/nvim-colorizer.lua",
-	-- 	event = "BufRead",
-	-- 	config = function() require "plugins.colorizer" end }
+	-- fancy tab bar
+    use { "akinsho/nvim-bufferline.lua",
+		config = function() require "plugins.bufferline" end }
+
+    -- file picker dashboard
+	use { "glepnir/dashboard-nvim",
+		cmd = { "Dashboard",
+			"DashboardNewFile",
+			"DashboardJumpMarks",
+			"SessionLoad",
+			"SessionSave" },
+		setup = function() require "plugins.dashboard" end }
+
+-- -- PART 2
+
+	-- -- Extra lua functions needed by some plugins
+	-- use { "nvim-lua/plenary.nvim",
+	-- 	event = "BufRead" }
+	
+	-- -- Small popup window library
+	-- use { "nvim-lua/popup.nvim",
+	-- 	after = "plenary.nvim" }
+	
+	-- -- fuzzy file finder window
+	-- use { "nvim-telescope/telescope.nvim",
+	-- 	cmd = "Telescope",
+	-- 	config = function() require "plugins.telescope" end }
+
+        -- -- use { "nvim-telescope/telescope-fzf-native.nvim",
+        -- --     run = "make",
+        -- --     cmd = "Telescope" }
+        -- -- use { "nvim-telescope/telescope-media-files.nvim",
+        -- --     cmd = "Telescope" }
+	
+
+
+	-- -- alternative to telescope for when it doesnt work
+	-- use { "liuchengxu/vim-clap" }
+
+-- -- PART 3
+
 
 	-- -- code formatter
 	-- use { "sbdchd/neoformat",
