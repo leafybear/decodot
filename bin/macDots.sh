2a351b82189576840c77a68fa72203f7142df38b
Amy Bentley <leafybear@icloud.com>
2022-03-18 20:41:49 +1100

    combine linux and mac dot scripts into quickDots

--- bin/macDots.sh
+++
@@ -1,9 +0,0 @@
-python3 $DOTDIR/decodot $1 \
-bin \
-git \
-ranger \
-tmux \
-nvim \
-nnn \
-gitui \
-shell \
---
+++ bin/quickDots.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# a quick decodot setup script for my linux machines
+
+system=
+
+# Use the correct options on macOS and Linux
+if [[ `uname` == Darwin ]]; then
+	DOTDIR=$HOME/all/dots
+	system="macOS"
+else
+	DOTDIR=$HOME/dots
+	system="linux"
+fi
+
+export DOTDIR=$DOTDIR
+cd $DOTDIR
+
+case $system in
+	linux)
+		python3 decodot.py $1 apps bin bspwm cheat deco-themes dunst git htop kitty mpv neofetch picom polybar qimgv ranger rofi shell sxhkd tmux x nnn nvim gitui
+		;;
+	macOS)
+		python3 decodot $1 bin git gitui nvim nnn ranger shell tmux
+		;;
+	*)
+		echo "Couldn't match this system."
+		;;
+esac
